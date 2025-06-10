20604de0c9516284a79f1806cb36bff5713664c0
aahma <aahma@LAPTOP-HVKUOEJO>
2025-06-10 11:24:01 +0330

    Dockerize

--- app/car_manager.py
+++ app/car_manager.py
@@ -2,15 +2,26 @@ from car import Car
 import queries as q
 import psycopg2
 import helper as helper
+import os
 
 class CarManager:
     def __init__(self):     
+        # self.conn = psycopg2.connect(
+        #     dbname='car_db',
+        #     user='postgres',
+        #     password='76827633',
+        #     host='localhost',
+        #     port='5433',        
+        # )
+        # self.cursor = self.conn.cursor()
+        
+        
         self.conn = psycopg2.connect(
-            dbname='car_db',
-            user='postgres',
-            password='76827633',
-            host='localhost',
-            port='5433',        
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST", "localhost"),
+            port=os.getenv("DB_PORT", "5432")
         )
         self.cursor = self.conn.cursor()
         
